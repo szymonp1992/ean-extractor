fe2e025d1cafabdc67942d568c20e674cfe8d006
Szymon Podstufka <74513612+szymonp1992@users.noreply.github.com>
2022-07-28 09:33:41 +0200

    Create script.js
    Script for the functionality

---
+++ script.js
@@ -0,0 +1,27 @@
+const inputField = document.querySelector(".input");
+const outputField = document.querySelector(".output");
+
+let czechContent;
+
+inputField.addEventListener("input", (e) => {
+  czechContent = e.target.value;
+
+  const czechContentwords = czechContent.split(/[.\s]+/);
+
+  const numbers = [];
+
+  let withoutOne = "";
+
+  for (let i = 0; i < czechContentwords.length; i++) {
+    // Usuwanie -1 z końcówki EAN-u
+    if (czechContentwords[i].includes("-1")) {
+      withoutOne = czechContentwords[i].replace("-1", "");
+      numbers.push(withoutOne);
+    }
+    if (czechContentwords[i] > 11111) {
+      numbers.push(czechContentwords[i]);
+    }
+    const czechContentEans = numbers.toString();
+    outputField.value = czechContentEans;
+  }
+});
